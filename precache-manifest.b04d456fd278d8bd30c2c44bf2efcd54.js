89e77c76075d28d9140e9260db0786014c1c0cf4
Emmanuel <godsonlive91@gmail.com>
2019-11-23 17:53:02 +0000

    Updates

---
+++ precache-manifest.b04d456fd278d8bd30c2c44bf2efcd54.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31241cd87dd4c4dd3e69ced9e1ae69e1",
+    "url": "/portfolio-Ajebo/index.html"
+  },
+  {
+    "revision": "d023f82010564cc2c272",
+    "url": "/portfolio-Ajebo/static/css/main.b5e97f61.chunk.css"
+  },
+  {
+    "revision": "0b0041aefc02495edaa4",
+    "url": "/portfolio-Ajebo/static/js/2.416ca0de.chunk.js"
+  },
+  {
+    "revision": "d023f82010564cc2c272",
+    "url": "/portfolio-Ajebo/static/js/main.1049a6e1.chunk.js"
+  },
+  {
+    "revision": "0fe04556ee9b42ed8a37",
+    "url": "/portfolio-Ajebo/static/js/runtime~main.a01ea669.js"
+  },
+  {
+    "revision": "bbdf649d024de04534ce1fd377e9b1ab",
+    "url": "/portfolio-Ajebo/static/media/Headimg.bbdf649d.jpg"
+  },
+  {
+    "revision": "d98b6dd7fa9e8e61a98858725234dbf4",
+    "url": "/portfolio-Ajebo/static/media/a.d98b6dd7.jpg"
+  },
+  {
+    "revision": "fb92a0ed1b44e7917697a45bd08fffca",
+    "url": "/portfolio-Ajebo/static/media/alive.fb92a0ed.jpg"
+  },
+  {
+    "revision": "56b7a8158b903f0747c183bf3b5e574b",
+    "url": "/portfolio-Ajebo/static/media/be.56b7a815.jpg"
+  },
+  {
+    "revision": "f8ec22cb8ee26e3d4d99a8dd20482fbb",
+    "url": "/portfolio-Ajebo/static/media/bee.f8ec22cb.jpg"
+  },
+  {
+    "revision": "4061e56ad1080efd9e42e020679e4bc6",
+    "url": "/portfolio-Ajebo/static/media/book.4061e56a.jpg"
+  },
+  {
+    "revision": "faa6d9c605425af99677bb2eef352751",
+    "url": "/portfolio-Ajebo/static/media/calz.faa6d9c6.jpg"
+  },
+  {
+    "revision": "7027e37fa59e04756cf05b08ae06a1ae",
+    "url": "/portfolio-Ajebo/static/media/classic.7027e37f.jpg"
+  },
+  {
+    "revision": "43fa16019ddff8ba81bc139b5f08b38c",
+    "url": "/portfolio-Ajebo/static/media/cute.43fa1601.jpg"
+  },
+  {
+    "revision": "dd100d1bf0093326aefff7d0d04af1e2",
+    "url": "/portfolio-Ajebo/static/media/family.dd100d1b.jpg"
+  },
+  {
+    "revision": "7d33a8abdb9f61d86f77203c8bcc8ba1",
+    "url": "/portfolio-Ajebo/static/media/famous.7d33a8ab.jpg"
+  },
+  {
+    "revision": "b82a3cb39d1b4655627fd564d26c26c8",
+    "url": "/portfolio-Ajebo/static/media/fit.b82a3cb3.png"
+  },
+  {
+    "revision": "0ac6cef27d3e1ae662eb40b9acc28cb6",
+    "url": "/portfolio-Ajebo/static/media/forb.0ac6cef2.jpg"
+  },
+  {
+    "revision": "1fe80e233fbf211c174edd07c442fb2e",
+    "url": "/portfolio-Ajebo/static/media/fun.1fe80e23.jpg"
+  },
+  {
+    "revision": "3ac489f270294fa7510a8d8bc60ea819",
+    "url": "/portfolio-Ajebo/static/media/god.3ac489f2.jpg"
+  },
+  {
+    "revision": "b736f064a3455ee698ec618b582cf88a",
+    "url": "/portfolio-Ajebo/static/media/hangut.b736f064.jpg"
+  },
+  {
+    "revision": "f8ec22cb8ee26e3d4d99a8dd20482fbb",
+    "url": "/portfolio-Ajebo/static/media/j.f8ec22cb.jpg"
+  },
+  {
+    "revision": "18a0f32b73ddb073bf94d095399b7103",
+    "url": "/portfolio-Ajebo/static/media/kiss.18a0f32b.jpg"
+  },
+  {
+    "revision": "5b0659a49edd52f738db455d4a9c5732",
+    "url": "/portfolio-Ajebo/static/media/rep.5b0659a4.jpg"
+  },
+  {
+    "revision": "4648116bba941c68c904b83d28ba4684",
+    "url": "/portfolio-Ajebo/static/media/s.4648116b.jpg"
+  },
+  {
+    "revision": "50d2752dc1b8bba5f0f59e5554b00357",
+    "url": "/portfolio-Ajebo/static/media/script.50d2752d.jpg"
+  },
+  {
+    "revision": "cc8dc93e9edc7bd287e29854c8223155",
+    "url": "/portfolio-Ajebo/static/media/work.cc8dc93e.jpg"
+  },
+  {
+    "revision": "ae85ff4f86aa2af1d4d8e284d65b3181",
+    "url": "/portfolio-Ajebo/static/media/work2.ae85ff4f.jpg"
+  },
+  {
+    "revision": "f417d5f98aaffed6d7fcd6899f89d4a6",
+    "url": "/portfolio-Ajebo/static/media/work3.f417d5f9.jpg"
+  },
+  {
+    "revision": "54ff071178954652e4721e3b99b75159",
+    "url": "/portfolio-Ajebo/static/media/work4.54ff0711.jpg"
+  },
+  {
+    "revision": "6dc820406ebfd73f77be904b2a8be0d4",
+    "url": "/portfolio-Ajebo/static/media/work5.6dc82040.png"
+  },
+  {
+    "revision": "55619de0a04e553c789398c69a9a936d",
+    "url": "/portfolio-Ajebo/static/media/work6.55619de0.png"
+  },
+  {
+    "revision": "face8fffc7300f7e7c18cc5a1f7447cb",
+    "url": "/portfolio-Ajebo/static/media/work7.face8fff.png"
+  },
+  {
+    "revision": "877771d86f6fb3afc0e538854235c87d",
+    "url": "/portfolio-Ajebo/static/media/work8.877771d8.png"
+  },
+  {
+    "revision": "2593e3f43f6313de6fcb1e01c931bad9",
+    "url": "/portfolio-Ajebo/static/media/work9.2593e3f4.png"
+  }
+]);
